8a9c69104ee0d63cfa6cb774952ad1955fecb440
user.walid1921 <user.kouiderayadwalid@gmail.com>
2024-12-03 20:39:28 +0100

    error fixed

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const validateString = (value: unknown) => {
+  if (!value || typeof value !== "string") {
+    return false;
+  }
+  return true;
+};
+export const getErrorMessage = (error: unknown): string => {
+  let message: string;
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "Something went wrong";
+  }
+  return message;
+};
